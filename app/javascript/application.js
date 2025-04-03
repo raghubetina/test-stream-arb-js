9e40085583a59e429365a1c74c2c02a18558af9c
Raghu Betina <raghu@firstdraft.com>
2025-04-03 03:08:50 -0500

    Streaming jQuery with turbo_stream kinda works

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import jquery from "jquery"
+window.$ = window.jQuery = jquery;
+
+import { Turbo } from "@hotwired/turbo-rails"
+
+Turbo.StreamActions.eval = function() {
+  document.querySelector("body").append(this.templateContent)
+}
